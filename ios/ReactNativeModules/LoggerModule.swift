988e14e6dc5d287871cf579d401f69aafb4ddc85
Oscar Cheung <github@oscarcheung.me>
2023-02-20 17:31:06 +0800

    Add LoggerModule to iOS

---
+++ ios/ReactNativeModules/LoggerModule.swift
@@ -0,0 +1,17 @@
+//
+//  LoggerModule.swift
+//  ReactNativeHybrid
+//
+//  Created by Oscar Cheung on 20/02/2023.
+//
+
+@objc(LoggerModule)
+class LoggerModule: NSObject {
+  @objc
+  static func requiresMainQueueSetup() -> Bool { return true }
+
+  @objc
+  func log(_ message: String) -> Void {
+    print("LoggerModule:", message)
+  }
+}
